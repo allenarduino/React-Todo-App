e0d70672210d434777c31279192874e2d6d497cd
ALLEN <aljay3334@gmail.com>
2021-08-02 19:04:45 +0100

    some few changes

--- src/reducers/TaskReducer.js
+++ src/reducers/TaskReducer.js
@@ -2,15 +2,15 @@ import uuid from "uuid/v1";
 
 export const taskReducer = (state, action) => {
   switch (action.type) {
-    case ADD_TASK: {
+    case "ADD_TASK": {
       return [...state, action.payload];
     }
-    case CHECK_TASK: {
+    case "CHECK_TASK": {
       let taskIndex = state.findIndex(t => t.id === action.payload.id);
       state[taskIndex].isChecked = action.task.isChecked;
       return state.filter(task => task.id !== action.id);
     }
-    case REMOVE_TASK: {
+    case "REMOVE_TASK": {
       return state.filter(task => task.id !== action.id);
     }
     default:
